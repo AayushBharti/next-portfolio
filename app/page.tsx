d8fb374b1ae4325a359c4bce0bf795acf39541e9
Aayush Bharti <aayushbh2002@gmail.com>
2024-07-01 00:03:32 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,9 @@ import Grid from '@/components/Grid';
 import Hero from '@/components/Hero';
 import RecentProjects from '@/components/RecentProjects';
 import { FloatingNav } from '@/components/ui/FloatingNavbar';
+import { navItems } from '@/data';
 
 export default function Home() {
-  const navItems = [
-    {
-      name: 'Home',
-      link: '/',
-      // icon: <IconHome className='h-4 w-4 text-neutral-500 dark:text-white' />,
-    },
-    {
-      name: 'About',
-      link: '/about',
-      // icon: <IconUser className='h-4 w-4 text-neutral-500 dark:text-white' />,
-    },
-    {
-      name: 'Contact',
-      link: '/contact',
-      // icon: <IconMessage className='h-4 w-4 text-neutral-500 dark:text-white' />,
-    },
-  ];
 
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
